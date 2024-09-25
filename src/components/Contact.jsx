7306457078b6456628946984d5e1adc72a6a4f4f
subahan ali <subahanislam523@gmail.com>
2024-09-25 16:54:48 +0600

    added to nextjs project

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h2>This is Contact section 01786727749</h2>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Heading = () => {
+    return (
+        <div>
+            <h2>This is heading section</h2>
+        </div>
+    );
+};
+
+export default Heading;
